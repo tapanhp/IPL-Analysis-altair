17f90d2bed054a8189a28c4cd409fb1ca1f27d96
TruthSeekerBeast <truthseekerbeast@gmail.com>
2019-05-06 19:32:05 +0530

    batsman vs team without filter done

--- ipl_dvs/players/views.py
+++ ipl_dvs/players/views.py
@@ -18,6 +18,12 @@ def index(request):
     top_runs.sort_values('Runs', ascending=False, inplace=True)
     top_runs_vs_team = pd.read_csv('data_files/top_10_batsman_against_teams.csv', delimiter=',')
     top_runs_vs_team.sort_values('runs', ascending=False, inplace=True)
+    player_list = pd.read_csv('data_files/players.csv', delimiter=',')
+    player_list.sort_values('Player_Name', inplace=True)
+    team_list = pd.read_csv('data_files/teams.csv', delimiter=',')
+    team_list.sort_values('Team_Name', inplace=True)
+    batsman_vs_team = pd.read_csv('data_files/batsman_vs_team.csv', delimiter=',')
+    batsman_vs_team = batsman_vs_team[batsman_vs_team['player'] == 'V Kohli'][batsman_vs_team['team'] == 'Chennai Super Kings']
     top_runs_bars = alt.Chart(top_runs.head(10)).mark_bar().encode(
         x=alt.X('Player:N', sort=alt.EncodingSortField(field='Runs:Q', order='ascending')),
         y='Runs:Q',
@@ -50,15 +56,45 @@ def index(request):
             )
         )
     compound_chart = facet_wrap(subplts, plots_per_row=1)
+
+    highlight = alt.selection(type='multi', on='mouseover',
+                              fields=['season'], nearest=True)
+
+    batsman_vs_team_base = alt.Chart(batsman_vs_team).encode(
+        x='match:O',
+        y='runs:Q',
+        tooltip=['player', 'runs', 'season'],
+    )
+
+    batsman_vs_team_points = batsman_vs_team_base.mark_circle().encode(
+        opacity=alt.value(0),
+    ).add_selection(
+        highlight,
+    ).properties(
+        height=300,
+        width=900
+    )
+
+    batsman_vs_team_lines = batsman_vs_team_base.mark_line(point=True).encode(
+        opacity=alt.condition(~highlight, alt.value(1), alt.value(1)),
+        size=alt.condition(~highlight, alt.value(5), alt.value(5))
+    )
+    batsman_vs_team_chart = batsman_vs_team_points + batsman_vs_team_lines
+
+    print(batsman_vs_team_chart.to_json())
+
     context = {
         'top_score_data': top_runs_bars.to_json(),
         'top_score_vs_team_data': compound_chart.configure_line(
-                color='red'
-            ).configure_point(
+            color='red'
+        ).configure_point(
             color='green',
             filled=True,
             opacity=1
         ).to_json(),
-        'app_name': 'players'
+        'batsman_vs_team_data': batsman_vs_team_chart.to_json(),
+        'app_name': 'players',
+        'player_list': player_list['Player_Name'].tolist(),
+        'team_list': team_list['Team_Name'].tolist()
     }
     return render(request, 'players/index.html', context)
