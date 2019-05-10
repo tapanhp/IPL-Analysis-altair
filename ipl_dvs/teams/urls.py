99eb93db79c08c1455b9071584cce43bceca03af
TruthSeekerBeast <truthseekerbeast@gmail.com>
2019-05-10 12:58:23 +0530

    team vs team results done

--- ipl_dvs/teams/urls.py
+++ ipl_dvs/teams/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'teams'
 urlpatterns = [
     path('', views.index, name='home'),
-    path('update_top_wickets/', views.update_top_wickets, name='top_wickets')
+    path('update_top_wickets/', views.update_top_wickets, name='top_wickets'),
+    path('update_team_vs_team/', views.update_team_vs_team, name='team_vs_team'),
 ]
--- ipl_dvs/teams/views.py
+++ ipl_dvs/teams/views.py
@@ -14,7 +14,8 @@ def index(request):
     total_wickets = dr.total_wickets.to_html(index=False)
     wickets = dr.top_10_wicket[dr.top_10_wicket['team'] == 'Chennai Super Kings']
     wickets = wickets[['player', 'wickets']]
-    print(wickets)
+    team_vs_team = dr.team_vs_team[dr.team_vs_team['team'] == 'Chennai Super Kings']
+    team_vs_team = team_vs_team[['vs', 'wins', 'losses']].to_html(index=False)
     context = {
         'app_name': 'teams',
         'team_win_loss_data': win_loss_chart.to_json(),
@@ -22,13 +23,22 @@ def index(request):
         'total_runs_table': total_runs,
         'total_wickets_table': total_wickets,
         'top_ten_wickets_table': wickets.to_html(index=False),
+        'team_vs_team_table': team_vs_team,
     }
     return render(request, 'teams/index.html', context)
 
 
 @csrf_exempt
-def update_top_wickets(requset):
-    team = requset.POST['team']
+def update_top_wickets(request):
+    team = request.POST['team']
     wickets = dr.top_10_wicket[dr.top_10_wicket['team'] == team]
     wickets = wickets[['player', 'wickets']].to_html(index=False)
     return JsonResponse(wickets, safe=False)
+
+
+@csrf_exempt
+def update_team_vs_team(request):
+    team = request.POST['team']
+    team_vs_team = dr.team_vs_team[dr.team_vs_team['team'] == team]
+    team_vs_team = team_vs_team[['vs', 'wins', 'losses']].to_html(index=False)
+    return JsonResponse(team_vs_team, safe=False)
