19fa2a0a35506a8896a5afd5df24becc9c308848
TruthSeekerBeast <truthseekerbeast@gmail.com>
2019-05-10 12:58:23 +0530

    team vs team results done

--- ipl_dvs/teams/data_reader.py
+++ ipl_dvs/teams/data_reader.py
@@ -10,3 +10,5 @@ total_runs = pd.read_csv('data_files/team_total_runs.csv', delimiter=',')
 total_wickets = pd.read_csv('data_files/team_total_wickets.csv', delimiter=',')
 
 top_10_wicket = pd.read_csv('data_files/team_top_10_wicket_takers.csv', delimiter=',')
+
+team_vs_team = pd.read_csv('data_files/team_vs_team_wins.csv', delimiter=',')
