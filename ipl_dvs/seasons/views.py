fcdc983a26b30ba443df31cf9ccdc7c4243e2b47
TruthSeekerBeast <truthseekerbeast@gmail.com>
2019-05-20 15:36:13 +0530

    top scores and wicket takers done.

--- ipl_dvs/seasons/views.py
+++ ipl_dvs/seasons/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from . import data_reader as dr
 
 
 # Create your views here.
 def index(request):
+    top_runs = dr.top_runs.to_html(index=False)
+    top_wickets = dr.top_wickets.to_html(index=False)
     context = {
-        'app_name': 'seasons'
+        'app_name': 'seasons',
+        'top_runs': top_runs,
+        'top_wickets': top_wickets,
     }
     return render(request, 'seasons/index.html', context)
