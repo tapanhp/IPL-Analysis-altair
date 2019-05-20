f721b191f872c0d593742ebb9c6ab2fe5135bdea
TruthSeekerBeast <truthseekerbeast@gmail.com>
2019-05-20 16:34:10 +0530

    website review commit

--- ipl_dvs/seasons/data_reader.py
+++ ipl_dvs/seasons/data_reader.py
@@ -3,3 +3,5 @@ import pandas as pd
 top_runs = pd.read_csv('data_files/season_top_runs.csv', delimiter=',')
 
 top_wickets = pd.read_csv('data_files/season_top_wickets.csv', delimiter=',')
+
+team_wins = pd.read_csv('data_files/season_team_wins.csv', delimiter=',')
