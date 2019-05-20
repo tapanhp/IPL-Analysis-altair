af98647d6760da80d22ef03ddff42885bf282f1c
TruthSeekerBeast <truthseekerbeast@gmail.com>
2019-05-20 16:34:10 +0530

    website review commit

--- ipl_dvs/seasons/urls.py
+++ ipl_dvs/seasons/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'seasons'
 urlpatterns = [
     path('', views.index, name='home'),
+    path('update_team_wins/', views.update_team_wins, name='update_team_wins')
 ]
--- ipl_dvs/seasons/views.py
+++ ipl_dvs/seasons/views.py
@@ -1,4 +1,7 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
 from . import data_reader as dr
 
 
@@ -6,9 +9,19 @@ from . import data_reader as dr
 def index(request):
     top_runs = dr.top_runs.to_html(index=False)
     top_wickets = dr.top_wickets.to_html(index=False)
+    team_wins = dr.team_wins[dr.team_wins['season'] == 1][['team', 'wins']].sort_values('wins', ascending=False).to_html(index=False)
     context = {
         'app_name': 'seasons',
         'top_runs': top_runs,
         'top_wickets': top_wickets,
+        'team_wins': team_wins,
     }
     return render(request, 'seasons/index.html', context)
+
+
+@csrf_exempt
+def update_team_wins(request):
+    season = request.POST['season']
+    team_wins = dr.team_wins[dr.team_wins['season'] == int(season)][['team', 'wins']].sort_values('wins', ascending=False).to_html(
+        index=False)
+    return JsonResponse(team_wins, safe=False)
