4180088776e3f40c3b4b334f32f61ff8d06d1f46
TruthSeekerBeast <truthseekerbeast@gmail.com>
2019-05-21 15:12:59 +0530

    deploy branch initial

--- ipl_dvs/ipl_dvs/urls.py
+++ ipl_dvs/ipl_dvs/urls.py
@@ -13,13 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+
+from ipl_dvs import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('dashboard/', include('dashboard.urls')),
     path('players/', include('players.urls')),
     path('teams/', include('teams.urls')),
-    path('seasons/', include('seasons.urls'))
+    path('seasons/', include('seasons.urls')),
 ]
