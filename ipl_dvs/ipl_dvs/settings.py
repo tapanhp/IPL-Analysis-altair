b55f2d9f94f9091115646da9325443dedf29a801
TruthSeekerBeast <truthseekerbeast@gmail.com>
2019-05-21 15:12:59 +0530

    deploy branch initial

--- ipl_dvs/ipl_dvs/settings.py
+++ ipl_dvs/ipl_dvs/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '@y76ncclh3bs(39sbtc6*9sb1b)_1k^jj5kd@^-snegefw^1(='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
