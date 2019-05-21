2885621000206829579d4ae46cb18abc788b1cb5
TruthSeekerBeast <truthseekerbeast@gmail.com>
2019-05-21 16:41:21 +0530

    static root changes

--- ipl_dvs/ipl_dvs/settings.py
+++ ipl_dvs/ipl_dvs/settings.py
@@ -120,8 +120,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/var/www/html/ipl-analysis-altair/ipl_dvs/static'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'ipl_dvs/static')
