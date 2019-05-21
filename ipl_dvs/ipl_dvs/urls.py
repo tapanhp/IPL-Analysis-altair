63059bf664cf213cbdf4ccb3139e3447cc671e16
TruthSeekerBeast <truthseekerbeast@gmail.com>
2019-05-21 16:41:21 +0530

    static root changes

--- ipl_dvs/ipl_dvs/urls.py
+++ ipl_dvs/ipl_dvs/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -25,4 +26,4 @@ urlpatterns = [
     path('players/', include('players.urls')),
     path('teams/', include('teams.urls')),
     path('seasons/', include('seasons.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
